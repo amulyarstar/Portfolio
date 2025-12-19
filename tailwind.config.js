fafd90a441f48f96be2f6481bd0e63a10ceeb07a
Amulya  R <amulyar122006@example.com>
2025-12-19 11:54:12 +0530

    Initial portfolio website

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html"], // ⬅️ THIS IS THE IMPORTANT CHANGE
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
